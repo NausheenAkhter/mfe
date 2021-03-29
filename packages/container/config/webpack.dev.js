771d417bddd394a2a8d64493cb8b34419bc254ea
Nausheen Akhter <nausheen.akhter@ril.com>
2021-03-29 14:36:01 +0530

    added yaml file

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,8 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json')
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -12,15 +11,13 @@ const devConfig = {
       index: 'index.html',
     },
   },
-  plugins: [ new ModuleFederationPlugin({
-    name: 'container',
-    remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-    },
-    shared: packageJson.dependencies
-  }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
     }),
   ],
 };
