2b524fe2d19b2b3b4080c5ceb9a157bf75ac973d
Nausheen Akhter <nausheen.akhter@ril.com>
2021-04-09 14:40:55 +0530

    added auth project

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import MarketingApp from './components/MarketingApp'
+import React, {lazy, Suspense} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-
+import Progress from './components/progress'
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 })
+
+const MarketingLazy = lazy(() => 
+  import('./components/MarketingApp')
+)
+
+const AuthLazy = lazy(() => 
+  import('./components/AuthApp')
+)
 
 export default () => {
   return (
@@ -14,7 +21,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path='/auth' component={AuthLazy} />
+              <Route path='/' component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
